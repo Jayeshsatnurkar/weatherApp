d3096c95c48bb4f1236256504d0b6e9af071b30a
[jayesh satnurkar] <[jayeshsatnurkar1@gmail.com]>
2024-08-19 14:52:07 +0530

    creating

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NewApp from './components/NewApp.jsx';
+
+
+import  '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
+  function App(){
+    return(
+        <>
+        <NewApp/>   
+
+        </>
+    )
+}
+
+export default App;
